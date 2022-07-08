81ade049ef7b0111051a2c336908a88a01185a25
amchelapurath2023 <anshul@chelapurath.com>
2022-07-08 19:34:43 -0400

    rename file

---
+++ app.py
@@ -0,0 +1,19 @@
+from flask import Flask
+from flask import Flask, flash, redirect, render_template, request, session, abort
+import os
+
+citylogin = Flask(__name__)
+
+@citylogin.route('/login', methods=['GET', 'POST'])
+def login():
+    error = None
+    if request.method == 'POST':
+        if request.form['username'] != 'admin' or request.form['password'] != 'admin':
+            error = 'Invalid Credentials. Please try again.'
+        else:
+            return redirect(url_for('home'))
+    return render_template('login.html', error=error)
+
+if __name__ == "__main__":
+    citylogin.secret_key = os.urandom(12)
+    citylogin.run(host='0.0.0.0', port=8080)
