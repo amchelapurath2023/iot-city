c57a489a9fff936bc465ed5186d46f6d515ea73d
amchelapurath2023 <anshul@chelapurath.com>
2022-07-08 19:23:26 -0400

    update login

--- citylogin.py
+++ citylogin.py
@@ -4,20 +4,15 @@ import os
 
 app = Flask(__name__)
 
-@app.route("/")
-def home():
-    if not session.get('logged_in'):
-        return render_template('login.html')
-    else:
-        return "Hello Boss!"
-
-@app.route('/login', methods=['POST'])
-def do_admin_login():
-    if request.form['password'] == 'password' and request.form['username'] == 'admin':
-        session['logged_in'] = True
-    else:
-        flash('wrong password!')
-    return home() 
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    error = None
+    if request.method == 'POST':
+        if request.form['username'] != 'admin' or request.form['password'] != 'admin':
+            error = 'Invalid Credentials. Please try again.'
+        else:
+            return redirect(url_for('home'))
+    return render_template('login.html', error=error)
 
 if __name__ == "__main__":
     app.secret_key = os.urandom(12)
